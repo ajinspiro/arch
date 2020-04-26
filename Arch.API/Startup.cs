2e70ee0397dac898a0984813497aa13021d6d5e3
ajinspiro <ajarunkumar96@gmail.com>
2020-04-27 00:48:10 +0530

    Swagger changes

--- Arch.API/Startup.cs
+++ Arch.API/Startup.cs
@@ -2,10 +2,12 @@ using Arch.API;
 using Arch.Injection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace arch
 {
@@ -25,7 +27,6 @@ namespace arch
             services.AddSwaggerGen(options =>
             {
                 options.SwaggerDoc("v1", new OpenApiInfo { Title = "Arch API" });
-
             });
 
             services.Configure<AppSettings>(Configuration);
@@ -56,6 +57,8 @@ namespace arch
             {
                 options.SwaggerEndpoint("/swagger/v1/swagger.json", "Arch API V1");
                 options.RoutePrefix = string.Empty;
+                options.DocumentTitle = "Swagger UI - Arch API";
+                options.DisplayRequestDuration();
             });
         }
     }
